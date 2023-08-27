b194f83b5ded2732256b63a6f8708808fd1f44fb
miguelgargallo <it@itamaesan.org>
2023-08-27 09:36:25 +0200

    gh 0.0.22 Create folders

---
+++ 00_Begin_CSHARP/22_AutoSetup.cs
@@ -0,0 +1,34 @@
+using Donations.Patreon
+using Donations.Patreon.Today;
+using Internet;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player; 
+
+    /* This code is premium content, the result of many hours of work and dedication. 
+       Your support through Patreon subscriptions will allow us to keep progressing and 
+       maintaining the YouTube program. This block of code is restricted to ensure a minimum level of exclusivity.
+       https://www.patreon.com/mgar
+    */
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    /* Aquest codi és contingut premium, resultat de moltes hores de treball i dedicació. 
+       El vostre suport mitjançant subscripcions a Patreon ens permetrà seguir avançant i 
+       mantenint el programa a YouTube. Aquest bloc de codi està restringit per assegurar un mínim nivell d'exclusivitat.
+       https://www.patreon.com/mgar
+    */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    /* Este código es contenido premium, resultado de muchas horas de trabajo y dedicación.
+       Su apoyo a través de suscripciones en Patreon nos permitirá seguir avanzando y manteniendo el programa en YouTube.
+       Este bloque de código está restringido para asegurar un mínimo nivel de exclusividad.
+       https://www.patreon.com/mgar
+    */
+    }
+}
