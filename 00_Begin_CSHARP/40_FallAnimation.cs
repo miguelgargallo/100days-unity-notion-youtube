2e420f261d6b5dee5eaeae8b1d54e690d579c9bc
miguelgargallo <it@itamaesan.org>
2023-10-02 18:13:53 +0200

    40 Fall Animation

---
+++ 00_Begin_CSHARP/40_FallAnimation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier = 1.5f;
+    public bool isOnGround = true;
+    public bool gameOver;
+    private Animator playerAnim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+        }
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        } else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over!");
+            gameOver = true;
+            // Set the animation to the death animation
+            playerAnim.SetBool("Death_b", true);
+            // Set the death type to the death type
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+    }
+}
+
